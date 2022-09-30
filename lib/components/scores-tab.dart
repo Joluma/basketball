b4d46d694c7b094c436eb62dc9bbc00a5a44fa3c
Joluma <j.mafuta@gmail.com>
2022-10-01 00:19:24 +0900

    style: flutter format scores tab

--- lib/components/scores-tab.dart
+++ lib/components/scores-tab.dart
@@ -31,29 +31,37 @@ class NbaScoresTabState extends State<NbaScoresTab> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: FutureBuilder<List>(
-      future: data,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return snapshot.data.length == 0
-              ? Center(
-                  child: Text('No games this day',
-                      style: TextStyle(fontSize: 24.0)))
-              : ListView.builder(
-                  itemCount: snapshot.data.length,
-                  padding: EdgeInsets.all(16.0),
-                  itemBuilder: (BuildContext context, int index) {
-                    return ScoreCard(snapshot.data[index]);
-                  });
-        } else if (snapshot.hasError) {
-          return Center(
-              child:
-                  Text('${snapshot.error}', style: TextStyle(fontSize: 24.0)));
-        }
+      child: FutureBuilder<List>(
+        future: data,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return snapshot.data.length == 0
+                ? Center(
+                    child: Text(
+                      'No games this day',
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: snapshot.data.length,
+                    padding: EdgeInsets.all(16.0),
+                    itemBuilder: (BuildContext context, int index) {
+                      return ScoreCard(snapshot.data[index]);
+                    },
+                  );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                '${snapshot.error}',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            );
+          }
 
-        // By default, show a loading spinner.
-        return Center(child: CircularProgressIndicator());
-      },
-    ));
+          // By default, show a loading spinner.
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
 }
